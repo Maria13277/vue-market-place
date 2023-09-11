371bcc3ae1d7d21335e4a5851e2eca0633132585
Maria <mariakovshova777@gmail.com>
2023-09-11 21:38:37 +0300

    Add router

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory} from 'vue-router'
+
+import HomeView from '@/views/HomeView.vue'
+import CatalogView from '@/views/CatalogView.vue'
+import ProductView from '@/views/ProductView.vue'
+import UpdateView from '@/views/UpdateView.vue'
+
+const router = createRouter ({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            redirect: { path: '/catalog' },
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/catalog',
+            name: 'catalog',
+            component: CatalogView
+        },
+        {
+            path: '/product/:id',
+            name: 'product',
+            component: ProductView
+        },
+        {
+            path: '/update/:id',
+            name: 'update',
+            component: UpdateView
+        }
+    ]
+})
+
+export default router
